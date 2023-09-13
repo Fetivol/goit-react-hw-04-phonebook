5419a59a4c1e6ca56fe7d87a59d072592f6a2324
Fetivol <Fetisowova@gmail.com>
2023-09-13 19:15:04 +0200

    Refactoring for hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Phonebook } from './Phonebook/Phonebook';
 import { ContactsList } from './ContactsList/ContactsList';
@@ -6,19 +6,17 @@ import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilters] = useState('');
 
-  addToContacts = contact => {
-    const existingContact = this.state.contacts.find(
+  const addToContacts = contact => {
+    const existingContact = contacts.find(
       c =>
         c.name.toLowerCase() === contact.name.toLowerCase() ||
         c.number === contact.number
@@ -27,66 +25,56 @@ export class App extends Component {
     if (existingContact) {
       alert('Contact already exists!');
     } else {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, { id: nanoid(), ...contact }],
-      }));
+      setContacts(prevContacts => [
+        ...prevContacts,
+        { id: nanoid(), ...contact },
+      ]);
     }
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  handleChange = filter => {
-    this.setState({
-      filter,
-    });
+  const handleChange = filter => {
+    setFilters(filter);
   };
 
-  componentDidMount() {
+  const visibleItems = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  useEffect(() => {
     const contacts = localStorage.getItem('contacts');
     const jsonParse = JSON.parse(contacts);
     if (jsonParse !== null) {
-      this.setState({ contacts: jsonParse });
+      setContacts(jsonParse);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  render() {
-    const { contacts, filter } = this.state;
+  return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <Phonebook onAdd={addToContacts}></Phonebook>
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChange={handleChange}></Filter>
 
-    const visibleItems = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+      {visibleItems.length > 0 ? (
+        <ContactsList
+          componentsData={visibleItems}
+          onDelete={deleteContact}
+        ></ContactsList>
+      ) : (
+        <span>We didn't find this person</span>
+      )}
 
-    return (
-      <Layout>
-        <h1>Phonebook</h1>
-        <Phonebook onAdd={this.addToContacts}></Phonebook>
-        <h2>Contacts</h2>
-        <Filter
-          filter={this.state.filter}
-          onChange={this.handleChange}
-        ></Filter>
-
-        {visibleItems.length > 0 ? (
-          <ContactsList
-            componentsData={visibleItems}
-            onDelete={this.deleteContact}
-          ></ContactsList>
-        ) : (
-          <span>We didn't find this person</span>
-        )}
-
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
+      <GlobalStyle />
+    </Layout>
+  );
+};
