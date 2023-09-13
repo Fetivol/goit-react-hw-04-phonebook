9434ca7c5e1b4e1c67314e68b511209c72fe98ae
Fetivol <Fetisowova@gmail.com>
2023-09-13 20:27:28 +0200

    Add defaultContacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,15 @@ import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
+  const defaultContacts = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? defaultContacts
+  );
   const [filter, setFilters] = useState('');
 
   const addToContacts = contact => {
